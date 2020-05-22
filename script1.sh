3564685d3fd8d3d43244f90cd03a3bbdbefff778
kshitijk83 <kshitijk83@gmail.com>
2020-05-22 17:06:24 +0530

    lerning about scope and exporting variables

---
+++ script1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# demonstrate variable scope 1
+
+var1=blah
+var2=foo
+
+# Let's verify their current value
+echo $0 :: var1 : $var1, var2 : $var2
+
+export var1
+./script2.sh
+
+echo $0 :: var1 : $var1, var2 : $var2
