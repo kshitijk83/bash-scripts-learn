76cb39d3f5fbb2f10efd83e906c545ecc087cedf
kshitijk83 <kshitijk83@gmail.com>
2020-05-27 19:56:11 +0530

    arithmatic

---
+++ arithmatic.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Basic arithmatic using let
+# spaces not required without quotes
+# with quotes you can write with spaces
+
+let a=4+5
+echo $a
+
+let "a = 5 + 4"
+echo $a
+
+let a++
+echo $a
+
+let "a = 4 * 5"
+echo $a
+
+let "a = $1 +30"
+echo $a
+
+# Basic arithmatic using expr
+# spaces are required and in quotes are not required
+expr 5 + 4
+
+expr 5 \* $1
+
+a=$(expr 10 - 3)
+echo $a
+
+# Basic aithmatic using double parentheses
+
+a=$((3+5))
+a=$(( 4 + 5 ))
+b=$((a+3))
+echo $b
+
+((b++))
